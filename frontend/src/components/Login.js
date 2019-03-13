1cb2b519d1fe432137af8284cfea739bc34af36a
sdcaulley <stephanie.caulley@gmail.com>
2019-03-13 16:19:55 -0400

    added Login componen

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import SingleInput from './SingleInput';
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        console.log(user);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Login</h2>
+                <form onSubmit={ this.handleSubmit }>
+                    <SingleInput
+                        type='email'
+                        placeholder='Email'
+                        name='email'
+                        controlFunc={ this.handleInputChange }
+                        content={ this.state.email } />
+                    <SingleInput
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        controlFunc={ this.handleInputChange }
+                        content={ this.state.password } />
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
