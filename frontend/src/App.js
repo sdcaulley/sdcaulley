7c40a3df76f06f2bd867dca5d4a4c95f1f61544e
sdcaulley <stephanie.caulley@gmail.com>
2019-03-13 17:16:29 -0400

    added store for redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
 import Login from './components/Login';
@@ -8,14 +10,17 @@ import Home from './components/Home';
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <Navbar />
-                    <Route exact path='/' component={ Home } />
-                    <Route exact path='/register' component={ Register } />
-                    <Route exact path='/login' component={ Login } />
-                </div>
-            </Router>
+            <Provider store = { store }>
+                <Router>
+                    <div>
+                        <Navbar />
+                        <Route exact path='/' component={ Home } />
+                        <Route exact path='/register' component={ Register } />
+                        <Route exact path='/login' component={ Login } />
+                    </div>
+                </Router>
+            </Provider>
+
         );
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers } from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -9,3 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default combineReducers({});
