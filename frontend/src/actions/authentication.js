93ad9ebac6233b6b3cb1a386b6ce25b8a7565721
sdcaulley <stephanie.caulley@gmail.com>
2019-03-20 16:57:27 -0400

    set token to local storage and created reducers/actions

--- frontend/src/actions/authentication.js
+++ frontend/src/actions/authentication.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export function registerUser(user, history) {
     return (dispatch) => {
         axios.post('/register', user)
             .then(res => history.push('/login'))
             .catch(err => {
+                console.log('err', err);
                 dispatch({
                     type: GET_ERRORS,
                     payload: err.response.data
@@ -18,7 +21,11 @@ export function loginUser(user) {
     return (dispatch) => {
         axios.post('/login', user)
             .then(res => {
-                console.log(res.data);
+                const { token } = res.data;
+                localStorage.setItem('jwtToken', token);
+                setAuthToken(token);
+                const decoded = jwt_decode(token);
+                dispatch(setCurrentUser(decoded));
             })
             .catch(err => {
                 dispatch({
@@ -26,5 +33,12 @@ export function loginUser(user) {
                     payload: err.response.data
                 });
             });
+    };
+}
+
+export function setCurrentUser(decoded) {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
     };
 }
