3fd367722dcb14f4e96a0b02a86924c70a61241f
sdcaulley <stephanie.caulley@gmail.com>
2019-06-24 11:33:22 -0500

    added nomalize.css to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authentication';
+import 'normalize.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Home from './components/Home';
