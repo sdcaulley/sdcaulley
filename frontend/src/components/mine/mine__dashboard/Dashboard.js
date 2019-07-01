861df4b99d9d0201370a3af9db25f7150c9a811a
sdcaulley <stephanie.caulley@gmail.com>
2019-07-01 17:22:27 -0500

    added placeholders for Dashboard components

--- frontend/src/components/mine/mine__dashboard/Dashboard.js
+++ frontend/src/components/mine/mine__dashboard/Dashboard.js
@@ -8,7 +8,7 @@ class Dashboard extends Component {
         super();
         this.onLogoutClick = this.onLogoutClick.bind(this);
     }
-    
+
     onLogoutClick(e) {
         e.preventDefault();
         this.props.logoutUser();
@@ -17,8 +17,27 @@ class Dashboard extends Component {
     render() {
         return (
             <React.Fragment>
-                <p>Hello Dashboard</p>
+                <h1>Dashboard</h1>
                 <button onClick={this.onLogoutClick}>Log Out</button>
+                <section>
+                    Create:
+                    <ul>
+                        <li>Blog Post</li>
+                        <li>Tag</li>
+                        <li>Favorite</li>
+                        <li>Project</li>
+                        <li>Quote</li>
+                    </ul>
+                </section>
+                <section>
+                    Draft:
+                    <ul>
+                        <li>Blog Post</li>
+                        <li>Favorite</li>
+                        <li>Project</li>
+                        <li>Quote</li>
+                    </ul>
+                </section>
             </React.Fragment>
         );
     }
