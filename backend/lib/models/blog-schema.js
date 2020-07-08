7df926008198c3b8b41c39ba4306d8b12616958e
sdcaulley <stephanie.caulley@gmail.com>
2020-07-08 13:03:28 -0400

    added dummy data for building

--- backend/lib/models/blog-schema.js
+++ backend/lib/models/blog-schema.js
@@ -1,39 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const BlogSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String
-  },
-  tag: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Tag',
-      required: true
-    }
-  ],
-  category: [
-    {
-      type: String,
-      enum: ['code', 'culture', 'craft'],
-      required: true
-    }
-  ],
-  date_created: {
-    type: Date,
-    default: Date.now
-  },
-  date_updated: {
-    type: Date,
-    default: Date.now
-  }
+	title: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	},
+	image: {
+		type: String
+	},
+	tag: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Tag',
+			required: true
+		}
+	],
+	category: [
+		{
+			type: String,
+			enum: ['code', 'culture', 'craft'],
+			required: true
+		}
+	],
+	date_created: {
+		type: Date
+	},
+	date_updated: {
+		type: Date
+	}
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
