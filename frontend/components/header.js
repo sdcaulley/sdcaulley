096c20bf7ac50e182e9a4c8076ba8994c0d9b7fd
sdcaulley <stephanie.caulley@gmail.com>
2020-08-18 14:05:14 -0400

    changed css to look more like sheets of paper

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -29,8 +29,7 @@ export default class Header extends LitElement {
 				header {
 					background-color: rgba(250, 240, 230, 0.7);
 					background-image: url('../css/ricepaper_v3.png');
-					border-style: outset;
-					border-width: 0.25em;
+					border-radius: 1em;
 					box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 					margin: 0.5em;
 					padding: 1em 3em;
--- frontend/css/blog-item-css.js
+++ frontend/css/blog-item-css.js
@@ -14,8 +14,7 @@ export const placement = css`
 	article {
 		background-color: rgba(250, 240, 230, 0.7);
 		background-image: url('../css/ricepaper_v3.png');
-		border-style: outset;
-		border-width: 0.25em;
+		border-radius: 1em;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 		margin: 0.5em;
 		padding: 0.5em;
